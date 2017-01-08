a12497f3d97ca5f3a1e14ecda20b6492f9edac10
ushiboy <ushiboy.dev@gmail.com>
2017-01-08 10:57:16 +0900

    add file read

--- wheels/cat/src/main.rs
+++ wheels/cat/src/main.rs
@@ -1,9 +1,34 @@
+use std::env;
 use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
+use std::fs::File;
 
 fn main() {
+
+    let arguments: Vec<String> = env::args().collect();
+
+    if arguments.len() < 2 {
+        let stdin = io::stdin();
+        let mut reader = stdin.lock();
+        output(&mut reader);
+    } else {
+        for file_path in &arguments[1..] {
+            let f = match File::open(file_path) {
+                Err(_) => panic!("none"),
+                Ok(file) => file,
+            };
+            let mut reader = BufReader::new(f);
+            output(&mut reader);
+        }
+    }
+
+}
+
+fn output(reader: &mut BufRead) {
     let mut input = String::new();
-    while io::stdin().read_line(&mut input).unwrap() > 0 {
+    while reader.read_line(&mut input).unwrap() > 0 {
         io::stdout().write(input.as_bytes()).unwrap();
         input.clear();
     }
